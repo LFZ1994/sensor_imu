60f1333e8bfcc8b6db942465d5a21da41b9c828f
LFZ1994 <lfz1994@qq.com>
2020-01-02 15:07:44 +0800

    add udev

---
+++ script/udev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="1218", MODE:="0777", GROUP:="dialout",  SYMLINK+="sensor_imu"' >/etc/udev/rules.d/sensor_imu_module.rules
+
+service udev reload
+sleep 1
+service udev restart
